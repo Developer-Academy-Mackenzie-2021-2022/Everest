6e466454faa5d06e76da9234661a0e308807b654
bia.duque <biaduque7@hotmail.com>
2021-10-28 14:23:10 -0300

    correcoes base grafico

--- FrameworkDeGraficos/Sources/FrameworkDeGraficos/Dispersao/DispersionGraph.swift
+++ FrameworkDeGraficos/Sources/FrameworkDeGraficos/Dispersao/DispersionGraph.swift
@@ -1,19 +1,24 @@
 import SwiftUI
 
-public struct DispersionGraph: View {
-    public typealias Body = <#type#>
-    
+public struct DispersionGraph {
+            
     // Construtor padrão
-    public init(_ data: [CGPoint]){
+    public init(_ data: [[CGFloat]]){
 
     }
-    
 }
 
+struct DispersionGraphView: View{
+    public var body: some View {
+        
+    }
+}
 
 struct DispersionGraph_Previews: PreviewProvider {
     static var previews: some View {
         VStack {
+            let data: [[CGFloat]] = [[10.0, 50.0, 30.0, 40.0, 100.0,60.0, 30.0,2.0], [2.0, 30.0, 90.0, 40.0, 0.0,60.0, 25.0,2.0]]
+            DispersionGraph(data)
         }
     }
 }
