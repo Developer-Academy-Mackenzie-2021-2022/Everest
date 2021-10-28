39598d2d500dfa84ce9c093feb2044c0290bd98b
VitorGK <vitorgk1@gmail.com>
2021-10-28 16:04:14 -0300

    Configurar primeiros passos

---
+++ FrameworkDeGraficos/Sources/FrameworkDeGraficos/Radar/RadarChart.swift
@@ -0,0 +1,9 @@
+import SpriteKit
+
+public struct RadarChart {
+    public private(set) var text = "Hello, World!"
+
+    public init(data: [CGPoint]) {
+        
+    }
+}
---
+++ FrameworkDeGraficos/Sources/FrameworkDeGraficos/Radar/RadarChartView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct RadarChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RadarChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        RadarChartView()
+    }
+}
