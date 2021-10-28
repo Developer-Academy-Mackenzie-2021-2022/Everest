c1d21e2cf785eb0dabf62c10dbe53afb8b58c75a
JulianaGSantana <jugsantana@icloud.com>
2021-10-28 16:42:47 -0300

    Create Variables

--- FrameworkDeGraficos/Sources/FrameworkDeGraficos/Area/AreaGraph.swift
+++ FrameworkDeGraficos/Sources/FrameworkDeGraficos/Area/AreaGraph.swift
@@ -1,6 +1,27 @@
-public struct AreaGraph {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
-    public init() {
+public struct AreaGraph: View {
+    public var datas: [[CGFloat]]
+    public var titulo: String
+    public var legendas: [String]
+    public var nomesEixoX: [String]
+    public var colors: [Color] = [Color.red]
+    
+    public init(datas: [[CGFloat]] = [[10.0,13.0,15.0]], titulo: String = "Title", legendas: [String] = ["legenda"], nomeseixoX: [String] = ["nome"]) {
+        self.datas = datas
+        self.titulo = titulo
+        self.legendas = legendas
+        self.nomesEixoX = nomeseixoX
+    }
+    public var body: some View {
+        Text("\(datas[0][0])")
+    }
+}
+
+struct AreaGraph_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack{
+            AreaGraph()
+        }
     }
 }
