047c4eff2ad2998e702d31fe88a065697ce375de
Bianca Maciel <macielmatosb@gmail.com>
2021-10-28 17:20:06 -0300

    matriz feita

--- FrameworkDeGraficos/Sources/FrameworkDeGraficos/Heatmap/HeatmapGraph.swift
+++ FrameworkDeGraficos/Sources/FrameworkDeGraficos/Heatmap/HeatmapGraph.swift
@@ -1,6 +1,35 @@
-public struct HeatmapGraph {
-    public private(set) var text = "Hello, World!"
+//
+//  TabelaView.swift
+//  HeatmapFramework
+//
+//  Created by Bianca Maciel Matos on 28/10/21.
+//
 
-    public init() {
+import SwiftUI
+
+struct HeatmapGraph: View {
+    
+    var matriz: [[Double]]
+    
+    var body: some View {
+        HStack{
+            ForEach((0 ..<  matriz.count), id:\.self){ i in
+                
+                VStack{
+                    ForEach(matriz[i], id:\.self){
+                        matriz in
+                            SquareView(quadrado: ModelSquare(valor: matriz, cor: Color.red, largura: 50.0, altura: 50.0))
+                        Text(String(format: "%.2f", matriz))
+                    }
+                }
+                
+            }
+        }
+    }
+}
+
+struct HeatmapGraph_Previews: PreviewProvider {
+    static var previews: some View {
+        HeatmapGraph(matriz: [[4,9,1], [1,1,1], [1,1,1]])
     }
 }
---
+++ FrameworkDeGraficos/Sources/FrameworkDeGraficos/Heatmap/View/SquareView.swift
@@ -0,0 +1,28 @@
+//
+//  SquareView.swift
+//  HeatmapFramework
+//
+//  Created by Bianca Maciel Matos on 28/10/21.
+//
+
+import SwiftUI
+
+struct SquareView: View {
+    var quadrado: ModelSquare
+
+    var body: some View {
+        quadrado.retangulo
+            .fill(quadrado.cor)
+            .frame(width: quadrado.largura, height: quadrado.altura)
+    }
+}
+
+struct SquareView_Previews: PreviewProvider {
+    static var previews: some View {
+        SquareView(
+        
+            quadrado: ModelSquare(valor: 2.0, cor: Color.red, largura: 100.0, altura: 100.0)
+        )
+            .previewLayout(.fixed(width: 100, height: 100))
+    }
+}
