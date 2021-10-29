a7b4a06aaf740a40a7bbde05c8253c804845ff5f
VitorGK <vitorgk1@gmail.com>
2021-10-29 17:53:11 -0300

    Update RadarLabelsView.swift

--- FrameworkDeGraficos/Sources/FrameworkDeGraficos/Radar/RadarLabelsView.swift
+++ FrameworkDeGraficos/Sources/FrameworkDeGraficos/Radar/RadarLabelsView.swift
@@ -1,38 +1,39 @@
-//
-//  SwiftUIView.swift
-//  
-//
-//  Created by Thais da Silva Bras on 29/10/21.
-//
-
 import SwiftUI
 import CoreGraphics
 
 struct RadarLabelsView: View {
-    var sides: Int
+    var labels: [String]
+    var size: Double
     
     var body: some View {
-        ZStack{
-            ForEach((0..<sides), id: \.self){
-                
+        ZStack {
+            ForEach((0..<labels.count), id: \.self) {
+                let pos = getLabelPosition(side: $0)
+                Text(labels[$0])
+                    .position(x: pos.x, y: pos.y)
             }
         }
     }
     
     private func getLabelPosition(side: Int) -> CGPoint {
-        let center: CGPoint = CGPoint(x: rect.width/2, y: rect.height/2)
-        let angle: Double = 2.0 * Double.pi / Double(sides)
-        for i in 0..<sides {
-            let x: Double = cos(Double(i) * angle) * (size)
-            let y: Double = sin(Double(i) * angle) * (size)
-            let point = CGPoint(x: x+center.x, y: y+center.y)
-            chartPoints.append(point)
-        }
+        let center: CGPoint = CGPoint(x: UIScreen.main.bounds.width/2, y: UIScreen.main.bounds.height/2)
+        let angle: Double = 2.0 * Double.pi / Double(labels.count)
+        let x: Double = cos(Double(side) * angle) * (size * 1.5)
+        let y: Double = sin(Double(side) * angle) * (size * 1.5)
+        return CGPoint(x: x + center.x, y: y + center.y)
     }
 }
 
 struct RadarLabelsView_Previews: PreviewProvider {
     static var previews: some View {
-        RadarLabelsView()
+        let labels: [String] = [
+            "Thatazinha",
+            "Muzinha",
+            "GKzinho",
+            "Algumazinha",
+            "Coisinha"
+        ]
+        RadarLabelsView(labels: labels, size: 100)
+            .rotationEffect(Angle(degrees: -90))
     }
 }
