6d370850d674e04454282843f17a3d81afd5a02a
João Victor Ferreira Pimenta <joaovictorfp30@gmail.com>
2021-10-29 17:57:49 -0300

    Relacionando Barras com Dados

--- FrameworkDeGraficos/Sources/FrameworkDeGraficos/FluxoDeCaixa/CashFlowView.swift
+++ FrameworkDeGraficos/Sources/FrameworkDeGraficos/FluxoDeCaixa/CashFlowView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct CashFlowView: View {
     
-    let data = [25.3, 30, 6, 7.4]
+    let data = [25.3, 30, 6, 7.4, 12.34]
     var body: some View {
         
         GeometryReader { bounds in
@@ -22,17 +22,26 @@ struct CashFlowView: View {
                 
                 VStack{
                     VStack{
-                        HStack{
+                        HStack(alignment: .bottom){
                             ForEach(0..<data.count) { index in
                                 //RoundedRectangle(cornerSize: CGSize(width: 5, height: data[index]))
                                 VStack {
                                     Text(String(format: "%.2f", data[index]))
-                                    Capsule()
+                                        .font(.caption)
+                                    LinearGradient(gradient: Gradient(colors: [Color.purple, Color.blue]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                                        .frame(height: CGFloat(data[index])*20)
+                                        .clipShape(Capsule())
+                                        .padding(.horizontal, 5)
                                 }
                             }
                         }
                         .padding()
                     }
+                    .background(
+                        Color.white
+                            .cornerRadius(25.0)
+                            .opacity(0.3)
+                    )
                     .frame(width: bounds.size.width-50, height: bounds.size.width)
                     
                 }
