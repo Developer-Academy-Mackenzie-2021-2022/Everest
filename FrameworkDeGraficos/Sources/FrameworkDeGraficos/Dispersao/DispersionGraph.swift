c354dc00bcaa4ff85909198133ef36bb17c9be57
bia.duque <biaduque7@hotmail.com>
2021-10-29 14:43:28 -0300

    Escalabilizar os pontos no grafico

--- FrameworkDeGraficos/Sources/FrameworkDeGraficos/Dispersao/DispersionGraph.swift
+++ FrameworkDeGraficos/Sources/FrameworkDeGraficos/Dispersao/DispersionGraph.swift
@@ -4,15 +4,25 @@ import SwiftUI
 struct DispersionGraph: View {
 
     public var points: [CGPoint]
+    public var maxValueX: CGFloat = 0
+    public var maxValueY: CGFloat = 0
+    
+    let max: CGFloat = 100
+    let min = 0
+
+    public mutating func setScale(data: [CGPoint]){
+        self.maxValueX = data.map {$0.x}.max() ?? CGFloat(0)
+        self.maxValueY = data.map {$0.y}.max() ?? CGFloat(0)
+    }
+    
     // Construtor padrão
     public init(_ data: [CGPoint]){
         self.points = data
+        //setar a escala correta nos pontos
+        setScale(data: points)
         
     }
         var body: some View {
-            let max: CGFloat = 100
-            let min = 0
-            
             
             GeometryReader { geometry in
                 let height = geometry.size.height
@@ -20,55 +30,40 @@ struct DispersionGraph: View {
                 
                 let centerY = height/2
                 
+                let scaleY = max/(maxValueY)
+                let scaleX = (centerY-max)/(maxValueX)
+                
                 let colors: [Color] = [Color.green, Color.blue, Color.red]
 
                 
-                HStack {
-                    VStack {
-                        ForEach((1...10).reversed(), id: \.self) {
-                            Text("\($0)")
-                        }
-                    }
-                    
-                    Spacer()
-                    
+                ZStack(alignment: .leading){
+                    Text("Eixo Y")
+                        .position(x: 30, y: centerY-max-10)
+                    Text("Eixo X")
+                        .position(x: width+10, y: centerY+10)
                     Path { path in
-                        path.move(to: CGPoint(x: 30, y: centerY-(max*1.2)))
+                        path.move(to: CGPoint(x: 30, y: centerY-max))
                         
-                        path.addLine(to: CGPoint(x: 30, y: centerY+max))
+                        path.addLine(to: CGPoint(x: 30, y: centerY))
                         
-                        path.addLine(to: CGPoint(x: width-40, y: centerY+max))
-
+                        path.addLine(to: CGPoint(x: width+10, y: centerY))
                     }.stroke()
-                    
-                    Spacer()
-                    
-                    HStack {
-                        ForEach((1...10).reversed(), id: \.self) {
-                            Text("\($0)")
-                        }
-                    }.padding(.top, 300).padding(.leading, -300)
-
-                    
-                }.padding()
+                }
                 
-                
-               
-               
-                
-                HStack{
+                ZStack{
                     ForEach(0..<points.count, id: \.self) { i in
+                        //let data = datas[$0]
+                        //let normalizedValuex = 1-((max-points[i].x)/(max-min))
                         
                         VStack{
                             Circle()
-                                .fill(colors[0])
+                                .fill(Color.blue)
                                 .frame(width: 10, height: 10, alignment: .center)
-                                .position(x: (points[i].x), y: (centerY-points[i].y))
-
-
-                        }.padding(-5)
+                                .position(x: CGFloat((points[i].x)*scaleX+30), y: centerY-CGFloat(points[i].y)*scaleY)
+                            
+                        }
                         Spacer()
-                    }.padding(-5)
+                    }
                 }
             }
         }
