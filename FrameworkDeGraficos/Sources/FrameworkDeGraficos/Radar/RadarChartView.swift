59efa2fa667a6d5eec34518aa98906aea1bc5bd5
VitorGK <vitorgk1@gmail.com>
2021-10-29 10:38:23 -0300

    Arrumar criação do polígono irregular através dos dados

--- FrameworkDeGraficos/Sources/FrameworkDeGraficos/Radar/RadarChartView.swift
+++ FrameworkDeGraficos/Sources/FrameworkDeGraficos/Radar/RadarChartView.swift
@@ -1,13 +1,47 @@
 import SwiftUI
 
 struct RadarChartView: View {
+    var data: [CGPoint]
+    var size: Double
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Radar(data: data)
+            .frame(width: size*2, height: size*2, alignment: .center)
+            .rotationEffect(Angle(degrees: -90))
+    }
+    
+    struct Radar: Shape {
+        let data: [CGPoint]
+        
+        func path(in rect: CGRect) -> Path {
+            var path = Path()
+            
+            var chartPoints: [CGPoint] = []
+            let center: CGPoint = CGPoint(x: rect.width/2, y: rect.height/2)
+            let angle: Double = 2.0 * Double.pi / Double(data.count)
+            for i in 0..<data.count {
+                let x: Double = cos(Double(i) * angle) * (data[i].y*rect.maxX)
+                let y: Double = sin(Double(i) * angle) * (data[i].y*rect.maxY)
+                let point = CGPoint(x: x+center.x, y: y+center.y)
+                chartPoints.append(point)
+            }
+            path.addLines(chartPoints)
+            path.closeSubpath()
+            
+            return path
+        }
     }
 }
 
 struct RadarChartView_Previews: PreviewProvider {
     static var previews: some View {
-        RadarChartView()
+        let data: [CGPoint] = [
+            CGPoint(x: 0, y: 0.5),
+            CGPoint(x: 1, y: 0.5),
+            CGPoint(x: 2, y: 0.5),
+            CGPoint(x: 3, y: 0.5),
+            CGPoint(x: 4, y: 0.5),
+        ]
+        RadarChartView(data: data, size: 100)
     }
 }
