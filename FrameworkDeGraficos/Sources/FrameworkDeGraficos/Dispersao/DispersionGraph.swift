987e72daa1b221f03d8302a49c9479bfc71f8a40
Nathalia Papst <nathaliapapst@gmail.com>
2021-10-29 17:35:33 -0300

    Testar os círculos em uma struct

--- FrameworkDeGraficos/Sources/FrameworkDeGraficos/Dispersao/DispersionGraph.swift
+++ FrameworkDeGraficos/Sources/FrameworkDeGraficos/Dispersao/DispersionGraph.swift
@@ -1,22 +1,77 @@
 import SwiftUI
 
+public struct DrawImage: View {
+    private var points: [CGPoint]
+    private var scaleX: CGFloat
+    private var scaleY: CGFloat
+    private var centerY: CGFloat
+    public var colors: [Color]
+    
+    public init (points: [CGPoint], scaleX: CGFloat, scaleY: CGFloat, centerY: CGFloat) {
+        self.points = points
+        self.scaleX = scaleX
+        self.scaleY = scaleY
+        self.centerY = centerY
+        self.colors = [Color.green, Color.blue, Color.red]
+    }
+    
+    public var body: some View {
+        ZStack {
+            ForEach (0..<points.count, id: \.self) { i in
+                VStack {
+                    Circle()
+                        .fill(colors[i])
+                        .frame(width: 10, height: 10, alignment: .center)
+                        .position(x: CGFloat((points[i].x)*scaleX+50), y: centerY-CGFloat(points[i].y)*scaleY)
+                }
+                
+                let auxX = points[i].x*scaleX+CGFloat(50)
+                let auxY = centerY-points[i].y*scaleY
+                Text(String(format: "%.2f", points[i].x))
+                    .position(x: CGFloat(auxX), y:centerY + CGFloat(10))
+                    .font(.subheadline)
+                Text(String(format: "%.1f", points[i].y))
+                    .position(x:50-20 , y:CGFloat(auxY))
+                    .font(.subheadline)
+            }
+        }
+
+    }
+}
 
 struct DispersionGraph: View {
 
-    public var points: [CGPoint]
+    public var points: [[CGPoint]]
     public var maxValueX: CGFloat = 0
     public var maxValueY: CGFloat = 0
     
     let max: CGFloat = 200
     let min: CGFloat = 0
-
-    public mutating func setScale(data: [CGPoint]){
-        self.maxValueX = data.map {$0.x}.max() ?? CGFloat(0)
-        self.maxValueY = data.map {$0.y}.max() ?? CGFloat(0)
+    
+    
+    public mutating func setScale(data: [[CGPoint]]){
+        for i in 0..<data.count {
+            if i > 0 {
+                if self.maxValueX < data[i].map {$0.x}.max() ?? CGFloat(0) {
+                    self.maxValueX = data[i].map {$0.x}.max() ?? CGFloat(0)
+                }
+                
+                if self.maxValueY < data[i].map {$0.y}.max() ?? CGFloat(0) {
+                    self.maxValueY = data[i].map {$0.y}.max() ?? CGFloat(0)
+                }
+            }
+            
+            else {
+                self.maxValueX = data[i].map {$0.x}.max() ?? CGFloat(0)
+                self.maxValueY = data[i].map {$0.y}.max() ?? CGFloat(0)
+            }
+            
+        }
+        
     }
     
     // Construtor padrão
-    public init(_ data: [CGPoint]){
+    public init(_ data: [[CGPoint]]){
         self.points = data
         //setar a escala correta nos pontos
         setScale(data: points)
@@ -32,8 +87,6 @@ struct DispersionGraph: View {
                 
                 let scaleY = max/(maxValueY)
                 let scaleX = (centerY-max)/(maxValueX)
-                
-                let colors: [Color] = [Color.green, Color.blue, Color.red]
 
                 
                 ZStack(alignment: .leading){
@@ -57,23 +110,11 @@ struct DispersionGraph: View {
                         //let data = datas[$0]
                         //let normalizedValuex = 1-((max-points[i].x)/(max-min))
                         
-                        VStack{
-                            Circle()
-                                .fill(Color.blue)
-                                .frame(width: 10, height: 10, alignment: .center)
-                                .position(x: CGFloat((points[i].x)*scaleX+50), y: centerY-CGFloat(points[i].y)*scaleY)
-                            
-                        }
+                        DrawImage(points: points[i], scaleX: scaleX, scaleY: scaleY, centerY: centerY)
+                        
                         Spacer()
                         
-                        let auxX = points[i].x*scaleX+CGFloat(50)
-                        let auxY = centerY-points[i].y*scaleY
-                        Text(String(format: "%.2f", points[i].x))
-                            .position(x: CGFloat(auxX), y:centerY + CGFloat(10))
-                            .font(.subheadline)
-                        Text(String(format: "%.1f", points[i].y))
-                            .position(x:50-20 , y:CGFloat(auxY))
-                            .font(.subheadline)
+                        
                     }
                 }
             }
@@ -82,11 +123,14 @@ struct DispersionGraph: View {
 
 struct DispersionGraph_Previews: PreviewProvider {
     static var previews: some View {
-        let points: [CGPoint] = [CGPoint(x: 2, y: 3),
+        let points: [[CGPoint]] = [[CGPoint(x: 2, y: 3),
                                          CGPoint(x: 4, y: 3),
                                          CGPoint(x: 4, y: 8),
-                                         CGPoint(x: 8, y: 7),
-                ]
+                                         CGPoint(x: 8, y: 7)],
+                                  [CGPoint(x: 5, y: 2),
+                                        CGPoint(x: 6, y: 4),
+                                        CGPoint(x: 1, y: 8),
+                                        CGPoint(x: 3, y: 5)]]
         
        DispersionGraph(points)
         
