3a1e5078232aa7b6c71f13c62312d146b18212f5
VitorCheung <Vitorcheung@gmail.com>
2021-11-03 16:01:59 -0300

    Cores degrade

--- FrameworkDeGraficos/Sources/FrameworkDeGraficos/Heatmap/HeatmapGraph.swift
+++ FrameworkDeGraficos/Sources/FrameworkDeGraficos/Heatmap/HeatmapGraph.swift
@@ -11,6 +11,8 @@ struct HeatmapGraph: View {
     
     var matriz: [[Double]]
     var showNumero: Bool
+    var corMaisForte:Color = .init(red: 202, green: 9, blue: 0)
+    var corMaisFraca:Color = .init(red: 254, green: 237, blue: 225)
     
     var body: some View {
         let escala = maiorNumeroDaMatriz(matriz: matriz)
@@ -25,11 +27,12 @@ struct HeatmapGraph: View {
                             quadrado: ModelSquare(valor: matriz, cor: Color.red, largura: 100.0, altura: 100.0),
                             showNumero: showNumero, escala: escala)
 
-//                        Text(String(format: "%.2f", matriz))
-
                     }
                 }
             }
+            Rectangle()
+                .fill(LinearGradient(gradient: Gradient(colors: [.init(hue: 0, saturation: 1, lightness: 0.347, opacity: 1),.init(hue: 0.06944444444, saturation: 0.935, lightness: 0.939, opacity: 1)]), startPoint: .top, endPoint: .bottom))
+                .frame(width: 50, height: 320)
         }
     }
     
@@ -42,11 +45,33 @@ struct HeatmapGraph: View {
         }
         return maiorNumero
     }
-}
+    
 
+}
+extension Color {
+    init(hue: Double, saturation: Double, lightness: Double, opacity: Double) {
+        precondition(0...1 ~= hue &&
+                     0...1 ~= saturation &&
+                     0...1 ~= lightness &&
+                     0...1 ~= opacity, "input range is out of range 0...1")
+        
+        //From HSL TO HSB ---------
+        var newSaturation: Double = 0.0
+        
+        let brightness = lightness + saturation * min(lightness, 1-lightness)
+        
+        if brightness == 0 { newSaturation = 0.0 }
+        else {
+            newSaturation = 2 * (1 - lightness / brightness)
+        }
+        //---------
+        
+        self.init(hue: hue, saturation: newSaturation, brightness: brightness, opacity: opacity)
+    }
+}
 
 struct HeatmapGraph_Previews: PreviewProvider {
     static var previews: some View {
-        HeatmapGraph(matriz: [[156,2,3], [5,600,7], [990,10,13]], showNumero: true)
+        HeatmapGraph(matriz: [[56,27,36], [54,100,75], [100,90,73]], showNumero: true)
     }
 }
