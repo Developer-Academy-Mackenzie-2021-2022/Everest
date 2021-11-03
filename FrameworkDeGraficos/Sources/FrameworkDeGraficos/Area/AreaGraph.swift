1188ed88930eba9dd3d9df455d577885f7acd607
Luca Hummel <luca.hummel12@gmail.com>
2021-11-03 14:17:45 -0300

    Alterar margens dos textos

--- FrameworkDeGraficos/Sources/FrameworkDeGraficos/Area/AreaGraph.swift
+++ FrameworkDeGraficos/Sources/FrameworkDeGraficos/Area/AreaGraph.swift
@@ -91,7 +91,7 @@ public struct AreaGraph: View {
                     
                     // Desenho dos nomes do eixoX
                     ForEach(0..<nomesEixoX.count, id: \.self) {
-                        Text(nomesEixoX[$0]).position(x: espaco*CGFloat($0) + marginEsquerda, y: baseGrafico + marginDireita)
+                        Text(nomesEixoX[$0]).position(x: espaco*CGFloat($0) + marginEsquerda, y: baseGrafico + 25)
                             .font(.caption)
                         
                     }
@@ -99,7 +99,7 @@ public struct AreaGraph: View {
                     // Desenho dos valores no eixo Y
                     ForEach(0...quantidadeY, id: \.self) {
                         let espacamento: CGFloat = alturaGrafico/CGFloat(quantidadeY)
-                        Text("\(Int(self.maiorValor)/quantidadeY*$0)").position(x: marginDireita, y: baseGrafico - (espacamento * CGFloat($0)))
+                        Text("\(Int(self.maiorValor)/quantidadeY*$0)").position(x: marginEsquerda - 25, y: baseGrafico - (espacamento * CGFloat($0)))
                             .font(.caption)
                     }
                     
@@ -156,9 +156,9 @@ struct AreaGraph_Previews: PreviewProvider {
         
         ScrollView {
             VStack{
-                //AreaGraph(datas: [[-10,-25,43], [65, 12, 34], [98,43,54], [76,45,12]])
-                AreaGraph()
-                AreaGraph(datas: [[10.0, 60.0, 1, 12, 43, 65, 12, 87, 93, -100, 23, 54], [70.0, 54.0, 0.0, 43, 54, 100, 87, 69, 32, 65, 45, 67], [23,65,87,13,42, 54.0, 0.0, 43, -512, 0, 65, 23]], titulo: "Monitoramento dos Focos Ativos em São Paulo legal oba bacana", legendas: ["Queimada", "Árvores", "Agua"], nomeseixoX: ["jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"], alturaGrafico: 150)
+                AreaGraph(datas: [[540,225,434, 432, 123, 543], [654, 132, 344, 500, 123, 250]], nomeseixoX: ["Agua e sal", "Lasanha", "Macarrao", "Doce", "Espaguete", "Pizza"])
+                //AreaGraph()
+                AreaGraph(datas: [[10.0, 60.0, 1, 12, 43, 65, 12, 87, 93, -100, 23, 54], [70.0, 54.0, 30.0, 43, 54, 100, 87, 69, 32, 65, 45, 67], [23,65,87,13,42, 54.0, 20.0, 43, -512, 30, 65, 23]], titulo: "Monitoramento dos Focos Ativos em São Paulo legal oba bacana", legendas: ["Queimada", "Árvores", "Agua"], nomeseixoX: ["jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"], alturaGrafico: 150)
             }
         }
     }
