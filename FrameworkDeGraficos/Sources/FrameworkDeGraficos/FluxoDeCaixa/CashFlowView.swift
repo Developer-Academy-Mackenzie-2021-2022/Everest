455b240662e372de664d4fa7cbd0c7e9b2cf5b63
ortizfrancielly <ortizfrancielly@gmail.com>
2021-11-03 17:33:36 -0300

    Iniciando normalização de dados

--- FrameworkDeGraficos/Sources/FrameworkDeGraficos/FluxoDeCaixa/CashFlowView.swift
+++ FrameworkDeGraficos/Sources/FrameworkDeGraficos/FluxoDeCaixa/CashFlowView.swift
@@ -15,61 +15,48 @@ struct CashFlowView: View {
         
         GeometryReader { bounds in
             ZStack {
-                
-                LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .topLeading, endPoint: .bottomTrailing)
-                    .opacity(0.6)
-                    .edgesIgnoringSafeArea(.top)
-                    .edgesIgnoringSafeArea(.bottom)
-                Text(String(data[1]))
-                    .frame(maxWidth: .infinity, alignment: .leading)
-//                    .position(x: 0, y: data[1])
-//                Text(String(data[0]))
-//                    .frame(maxWidth: .infinity, alignment: .leading)
-//                Text(String(data[4]))
-//                    .frame(maxWidth: .infinity, alignment: .leading)
-//                Text(String(data[3]))
-//                    .frame(maxWidth: .infinity, alignment: .leading)
-//                Text(String(data[2]))
-//                    .frame(maxWidth: .infinity, alignment: .leading)
-                    VStack{
-                        HStack(alignment: .bottom){
+                VStack{
+                    HStack(alignment: .bottom){
+                        VStack{
                             ForEach(0..<data.count) { index in
-                                //RoundedRectangle(cornerSize: CGSize(width: 5, height: data[index]))
-                                VStack {
-                                    Text(String(format: "%.2f", data[index]))
-                                        .font(.caption)
-                                        
-                                    LinearGradient(gradient: Gradient(colors: [Color.purple, Color.blue]), startPoint: .topLeading, endPoint: .bottomTrailing)
-                                        .frame(height: CGFloat(data[index])*20)
-                                        .clipShape(Rectangle())
-//                                        .background(
-//                                            Color.red
-//                                        )
-                                   .padding(.horizontal, 5)
-                                    HStack{
-                                        Text(String(years[index]))
-//                                            .font(.caption)
-                                    }
+                                Text(String(format: "%.2f", data[index]))
+                                    .font(.caption)
+                            }
+                            Spacer()
+                        }
+                        
+                        
+                        ForEach(0..<data.count) { index in
+                            VStack {
+                                LinearGradient(gradient: Gradient(colors: [Color.purple, Color.blue]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                                    .frame(height: CGFloat(data[index])*2)
+                                    .clipShape(Rectangle())
+                                    .padding(.horizontal, 5)
+                                HStack{
+                                    Text(String(years[index]))
                                 }
                             }
                             
                         }
-                        .padding()
+                        
                     }
-                    .background(
-                        Color.white
-                            .cornerRadius(25.0)
-                            .opacity(0.3)
-                    )
-                    .frame(width: bounds.size.width-50, height: bounds.size.width)
-                    
+                    .padding()
                 }
+                .background(
+                    Color.white
+                        .cornerRadius(25.0)
+                        .opacity(0.3)
+                )
+            
                 
             }
+            
         }
     }
+}
 struct CashFlowChartView_Previews: PreviewProvider {
     static var previews: some View {
         CashFlowView()
+            .previewLayout(.fixed(width: 400, height: 400))
     }
 }
