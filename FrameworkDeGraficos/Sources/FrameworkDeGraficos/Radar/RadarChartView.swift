336c625473aff913b7a9086f41d4aaaa4d5bf277
VitorGK <vitorgk1@gmail.com>
2021-11-03 17:19:43 -0300

    Update RadarChartView.swift

--- FrameworkDeGraficos/Sources/FrameworkDeGraficos/Radar/RadarChartView.swift
+++ FrameworkDeGraficos/Sources/FrameworkDeGraficos/Radar/RadarChartView.swift
@@ -1,44 +1,52 @@
 import SwiftUI
+import SpriteKit
 
 struct RadarChartView: View {
     var data: [[Double]]
     var size: Double
     var colors: [Color]
     var gridSize: Int
+    var labels: [String]
     
-    var body: some View {
-        ZStack {
-            // Grid
-            RadarGridView(gridSize: gridSize, sides: data[0].count, size: size)
-            
-            ForEach(0..<data.count) {
-                Radar(data: data[$0])
-                    .stroke(colors[$0], lineWidth: 3)
-                    .frame(width: size, height: size, alignment: .center)
-                    .rotationEffect(Angle(degrees: -90))
-            }
-        }
+    public init(data: [[Double]],
+                size: Double,
+                colors: [Color] = [Color(red: 0xF5 / 255, green: 0x45 / 255, blue: 0x4F / 255),
+                                   Color(red: 0xF4 / 255, green: 0x96 / 255, blue: 0x3F / 255),
+                                   Color(red: 0x92 / 255, green: 0xBC / 255, blue: 0x75 / 255),
+                                   Color(red: 0x59 / 255, green: 0x77 / 255, blue: 0x8E / 255),
+                                   Color(red: 0xEF / 255, green: 0x72 / 255, blue: 0x41 / 255),
+                                   Color(red: 0xF7 / 255, green: 0xC5 / 255, blue: 0x63 / 255),
+                                   Color(red: 0x4D / 255, green: 0xA8 / 255, blue: 0x8D / 255),],
+                gridSize: Int,
+                labels: [String]) {
+        self.data = data
+        self.size = size
+        self.colors = colors
+        self.gridSize = gridSize
+        self.labels = labels
     }
     
-    struct Radar: Shape {
-        let data: [Double]
-        
-        func path(in rect: CGRect) -> Path {
-            var path = Path()
-            
-            var chartPoints: [CGPoint] = []
-            let center: CGPoint = CGPoint(x: rect.width/2, y: rect.height/2)
-            let angle: Double = 2.0 * Double.pi / Double(data.count)
-            for i in 0..<data.count {
-                let x: Double = cos(Double(i) * angle) * (data[i]*rect.maxX)
-                let y: Double = sin(Double(i) * angle) * (data[i]*rect.maxY)
-                let point = CGPoint(x: x+center.x, y: y+center.y)
-                chartPoints.append(point)
+    var body: some View {
+        HStack {
+            ZStack {
+                // Grid
+                RadarGridView(gridSize: gridSize, sides: data[0].count, size: size)
+                
+                // Lines
+                RadarGridAxis(sides: data[0].count, size: size)
+                    .stroke(.black)
+                
+                ForEach(0..<data.count) {
+                    RadarLine(data: data[$0])
+                        .stroke(colors[$0], lineWidth: 2)
+                        .background(RadarLine(data: data[$0]).fill(colors[$0].opacity(0.33)))
+                        .frame(width: size, height: size, alignment: .center)
+                }
+                
+                RadarLabelsView(labels: labels, size: size)
+                    .edgesIgnoringSafeArea(.vertical)
             }
-            path.addLines(chartPoints)
-            path.closeSubpath()
             
-            return path
         }
     }
 }
@@ -47,8 +55,19 @@ struct RadarChartView_Previews: PreviewProvider {
     static var previews: some View {
         let data: [[Double]] = [
             [0.8, 1, 0.6, 0.7, 0.9, 0.5],
-            [0.3, 0.4, 0.1, 0.7, 0.3, 0.8]
+            [0.3, 0.4, 0.1, 0.7, 0.3, 0.8],
+            [0.2, 0.1, 0.6, 0.5, 0.9, 0.4],
+            [0.8, 0.9, 0.4, 0.3, 0.4, 0.6],
+            [0.4, 0.6, 0.9, 0.1, 0.5, 0.7],
         ]
-        RadarChartView(data: data, size: 100, colors: [.red, .blue], gridSize: 4)
+        let labels: [String] = [
+            "Strength",
+            "Constitution",
+            "Wisdom",
+            "Intelligence",
+            "Charisma",
+            "Dexterity"
+        ]
+        RadarChartView(data: data, size: 100, gridSize: 4, labels: labels)
     }
 }
