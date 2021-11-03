9f54fd99061b9323d5db0a4e31c20ae8969aee4e
Rebecca Mello <rebecca.mello@me.com>
2021-11-03 16:45:59 -0300

    Eixo Y ok

--- FrameworkDeGraficos/Sources/FrameworkDeGraficos/Velas/Views/CandleStickAxis.swift
+++ FrameworkDeGraficos/Sources/FrameworkDeGraficos/Velas/Views/CandleStickAxis.swift
@@ -28,76 +28,50 @@ struct CandleStickAxis<Child: View>: View {
     
     var body: some View {
         
-        HStack {
-//            let multiplierX: CGFloat = (1/CGFloat(xLabels.count))
-            let multiplierY: CGFloat = (1/CGFloat(yLabels.count))
-            
-            //MARK: Labels do eixo Y
-            HStack(alignment: .top) {
-                VStack {
-                    GeometryReader { proxy in
-                        ForEach(0..<(yLabels.count)) { i in
-                            let label: String = yLabels[i]
-                            let yPosition = (proxy.size.height*multiplierY) * CGFloat(i)
-                            Text("\(label)")
-                                .font(.system(size: 12))
-                                .offset(x: 12, y: yPosition-7)
-
-                        }
-                    }.frame(width: 50)
-                }.padding()
-            }
-            .frame(width: 40)
-            
-            
-            //MARK: Gráfico
-            VStack(alignment: .leading) {
-//                Text("\(yTitle)")
-                GeometryReader { proxy in
-                    ZStack {
-                        // Eixos Y
-                        ForEach(0..<(yLabels.count)) { i in
-                            Path { path in
+        GeometryReader { proxy in
+            HStack {
+                let multiplierY: CGFloat = (1/CGFloat(yLabels.count))
+                
+                //MARK: Labels do eixo Y
+                HStack(alignment: .top) {
+                    VStack {
+                        GeometryReader { proxy in
+                            ForEach(0..<(yLabels.count)) { i in
+                                let label: String = yLabels[i]
                                 let yPosition = (proxy.size.height*multiplierY) * CGFloat(i)
-                                path.move(to: CGPoint(x: proxy.size.width, y: yPosition))
-                                path.addLine(to: CGPoint(x: 0 , y: yPosition))
-                            }.stroke(Color.black.opacity(0.2))
-                        }
-                        
-                        axis(proxy.size)
-                        child()
-                        
-                        // Eixos X
-//                        ForEach(1..<(xLabels.count+1)) { i in
-//                            Path { path in
-//                                let xPosition = (proxy.size.width*multiplierX) * CGFloat(i)
-//                                path.move(to: CGPoint(x: xPosition, y: 0))
-//                                path.addLine(to: CGPoint(x:xPosition , y: proxy.size.height))
-//                            }
-//                        }
-                        
-                        
+                                Text("\(label)")
+                                    .font(.system(size: 12))
+                                    .offset(x: 12, y: yPosition-3)
+
+                            }
+                        }.frame(width: 50)
                     }
                 }
+                .frame(width: 40)
                 
-                //MARK: Labels do eixo X
-//                HStack {
-//                    GeometryReader { proxy in
-//                        ForEach(1..<(xLabels.count+1)) { i in
-//                            let label: String = xLabels[i-1]
-//                            let xPosition = (proxy.size.width*multiplierX) * CGFloat(i)
-//                            Text("\(label)")
-//                                .font(.system(size: 12))
-//                                .position(x: xPosition, y: proxy.size.height)
-//                        }
-//                    }.foregroundColor(.red)
-//                }.frame(height: 30)
-            }.padding()
-            
-            
-//            Text("\(xTitle)")
-//                .padding(.trailing, 20)
-        }.padding(.trailing, 20)
+                
+                //MARK: Gráfico
+                VStack(alignment: .leading) {
+    //                Text("\(yTitle)")
+                    GeometryReader { proxy in
+                        ZStack {
+                            // Eixos Y
+                            ForEach(0..<(yLabels.count)) { i in
+                                Path { path in
+                                    let yPosition = (proxy.size.height*multiplierY) * CGFloat(i)
+                                    path.move(to: CGPoint(x: proxy.size.width, y: yPosition))
+                                    path.addLine(to: CGPoint(x: 0 , y: yPosition))
+                                }.stroke(Color.black.opacity(0.2))
+                            }
+                            
+                            axis(proxy.size)
+                            child()
+                            
+                        }
+                    }
+                }
+            }
+        }.padding(.all, 4)
     }
 }
 
