cadcf7b3615a3f0b1e58a006f88b8acc0956c056
thallissousa <thallissousa@outlook.com>
2021-11-03 17:36:57 -0300

    Legenda adicionada.

--- FrameworkDeGraficos/Sources/FrameworkDeGraficos/Setores/PieChart.swift
+++ FrameworkDeGraficos/Sources/FrameworkDeGraficos/Setores/PieChart.swift
@@ -1,53 +1,76 @@
+//
+//  SwiftUIView.swift
+//
+//
+//  Created by Thallis Sousa on 03/11/21.
+//
+
 import SwiftUI
 
-struct PieChart: View {
-    @ObservedObject
-    var viewModel = PieChartViewModel(data: [0.5, 0.3, 0.1, 0.1],
-    cor: [.blue, .red, .yellow, .gray],
-    legenda: ["Legenda do Oi", "legenda vermelha", "legenda azul", "legenda cinza"])
-        
-    var body: some View {
+public struct PieChart: View {    
+    public var data: [Double] = []
+    public var label: [String] = []
+    public var cor: [Color] = []
+    public var legenda: [String] = []
+    
+    public init(data: [Double] = [0.27, 0.15, 0.18, 0.4],
+                cor:[Color] = [Color(red: 245 / 255, green: 69 / 255, blue: 79 / 255), Color(red: 146 / 255, green: 188 / 255, blue: 117 / 255), Color(red: 244 / 255, green: 150 / 255, blue: 63 / 255), Color(red: 89 / 255, green: 119 / 255, blue: 142 / 255)],
+                legenda:[String] = ["Agricultura", "Transporte", "Indústria", "Energia elétrica"]){
+        self.data = data
+        self.label = data.map{ value in "\(value*100)%" }
+        self.cor = cor
+        self.legenda = legenda
+    }
+    
+    
+    public var body: some View {
         ZStack {
-            ForEach(0..<viewModel.data.count) { index in
-                let currentData = viewModel.data[index]
+            ForEach(0..<data.count) { index in
+                let currentData = data[index]
                 let currentEndDegree =
                 currentData * 360
                 let lastDegree =
-                viewModel.data.prefix(index).map
+                data.prefix(index).map
                 { $0 }.reduce(0, +) *
                 360
                 
-              
-                ZStack {
-                    PieceOfPie(startDegree: lastDegree, endDegree: lastDegree + currentEndDegree)
-                        .fill(viewModel.cor[index])
+               
+                
+                VStack {
                     
                     
-                    GeometryReader { geometry in
-                        Text(viewModel.label[index]).font(.custom("System", size: 17))
-                            .foregroundColor(.white)
-                            .position(getLabelCoordinate(in: geometry.size, for: lastDegree + (currentEndDegree / 2)))
-                    
-//                        // Desenho das legendas
-//                                       HStack {
-//                                           ForEach(0..<legenda.count, id: \.self) {
-//                                               Rectangle()
-//                                                   .fill(colors[$0])
-//                                                   .frame(width: 10, height: 10)
-//                                               Text(legendas[$0]).font(.subheadline)
-//                                           }
-//                                       }.position(x: centerX, y: centerY+ContentView.frame)
-                                       
+                    ZStack {
+                        PieceOfPie(startDegree: lastDegree, endDegree: lastDegree + currentEndDegree)
+                            .fill(cor[index])
                         
                         
-                    }
-                    }
+                        GeometryReader { geometry in
+                            Text(label[index]).font(.custom("System", size: 17))
+                                .foregroundColor(.white)
+                                .position(getLabelCoordinate(in: geometry.size, for: lastDegree + (currentEndDegree / 2)))
+                                                    
+                                
+                            }
+                    } .frame(width: 300, height: 300, alignment: .center)
+                   
+                    // Desenho das legendas
+                    VStack(alignment: .leading) {
+                        ForEach(0..<legenda.count, id: \.self) {
+                            Rectangle()
+                                .fill(cor[$0])
+                                .frame(width: 10, height: 10)
+                           
+                            Text(legenda[$0]).font(.subheadline)
+                               
+                            
+                        }
+                    }                }
                 }
             }
         }
     }
-
-
+    
+    
     
     private func getLabelCoordinate(in geoSize: CGSize, for degree: Double)  -> CGPoint  {
         let center = CGPoint(x: geoSize.width / 2, y: geoSize.height / 2)
@@ -59,42 +82,24 @@ struct PieChart: View {
         return CGPoint(x: center.x + xCoordinate, y: center.y + yCoordinate)
         
     }
-
-
-
-
-struct ContentView: View {
-    var body: some View {
+    
+    
+    
+    
+    struct ContentView: View {
+        var body: some View {
             
             PieChart()
-        
+            
+        }
+    }
     
-            .frame(width: 300, height: 300, alignment: .center)
-            // .padding() //em relação à legenda
-                
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            
+            ContentView()
+            
+        }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-       
-        ContentView()
-        
-    }
-}
-
-final class PieChartViewModel: ObservableObject {
-
-    init(data: [Double], cor:[Color], legenda:[String]){
-        self.data = data
-        self.label = data.map{ value in "\(value*100)%" }
-        self.cor = cor
-        self.legenda = legenda
-    }
-   
-    var data: [Double] = []
-    var label: [String] = []
-    var cor: [Color] = []
-    var legenda: [String] = []
     
-}
+
