9da2b0c2659ed1823cf53eb7004d000191eca11d
caroltaus <carolinetaus@hotmail.com>
2021-11-03 17:18:28 -0300

    dataNormalizer para matriz

--- FrameworkDeGraficos/Sources/FrameworkDeGraficos/Linhas/Views/LineGraphContentView.swift
+++ FrameworkDeGraficos/Sources/FrameworkDeGraficos/Linhas/Views/LineGraphContentView.swift
@@ -21,19 +21,17 @@ struct LineGraphContentView: View {
         
         GeometryReader { proxy in
             ZStack {
-                
-                ForEach(lines, id: \.self) { line in
-                     
-                    let normalizedLine = dataNormalizer.normalizePoints(line.points, viewSize: proxy.size)
+                let normalizedData = dataNormalizer.normalizeLines(lines, viewSize: proxy.size)
+                ForEach(normalizedData, id: \.self) { line in
                     Path { path in
-                        guard let firstPoint = normalizedLine.first else {
+                        guard let firstPoint = line.points.first else {
                             return
                         }
                         
                         path.move(to: firstPoint)
                         
                         
-                        for point in normalizedLine {
+                        for point in line.points {
                             path.addLine(to: point)
                         }
                         
@@ -52,7 +50,7 @@ struct SwiftUIView_Previews1: PreviewProvider {
 
     static var previews: some View {
   
-        LineGraphContentView(lines: [Line(points: [CGPoint(x: 0, y: 0), CGPoint(x: 5, y: 9), CGPoint(x: 10, y: 10)], lineTitle: "linha", color: Color.blue, lineWidth: 3), Line(points: [CGPoint(x: 0, y: 0), CGPoint(x: 20, y: 20)], lineTitle: "linha", color: Color.red, lineWidth: 1)])
+        LineGraphContentView(lines: [Line(points: [CGPoint(x: 0, y: 0), CGPoint(x: 5, y: 9), CGPoint(x: 10, y: 10)], lineTitle: "linha", color: Color.blue, lineWidth: 3), Line(points: [CGPoint(x: 0, y: 0), CGPoint(x: 30, y: 20)], lineTitle: "linha", color: Color.red, lineWidth: 1)])
             .previewLayout(.fixed(width: 200.0, height: 200.0))
     }
 }
