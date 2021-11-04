b0f6b84a47692c4e461280c3f34eb2d377ab7508
VitorCheung <Vitorcheung@gmail.com>
2021-11-04 20:45:50 -0300

    ajustes finais

--- FrameworkDeGraficos/Sources/FrameworkDeGraficos/Heatmap/HeatmapGraph.swift
+++ FrameworkDeGraficos/Sources/FrameworkDeGraficos/Heatmap/HeatmapGraph.swift
@@ -18,7 +18,8 @@ struct HeatmapGraph: View {
     
     var body: some View {
         let escala = maiorNumeroDaMatriz(matriz: matriz)
-        let screenWidth = UIScreen.main.bounds.size.width * 0.6
+        let normalizador = (matriz.count>matriz[0].count ? matriz.count+1:matriz[0].count+1)
+        let screenWidth = UIScreen.main.bounds.size.width * 0.8
         
         
         VStack{
@@ -33,36 +34,39 @@ struct HeatmapGraph: View {
             }
             Rectangle()
                 .fill(LinearGradient(gradient: Gradient(colors: [.init(hue: 0, saturation: 1, lightness: 0.3, opacity: 1),.init(hue: 0, saturation: 1, lightness: 0.5, opacity: 1),.init(hue: 0, saturation: 1, lightness: 0.7, opacity: 1)]), startPoint: .trailing, endPoint: .leading))
-                .frame(width:(screenWidth*(3/2)) , height:screenWidth/(Double(matriz.count)*2))
+                .frame(width:(screenWidth*(12/11)) , height:screenWidth/(Double(normalizador)*2))
         
-        HStack{
-            VStack{
-                ForEach(legendaLinha, id:\.self){ legenda in
-                    Text(legenda)
-                        .frame(width: screenWidth/Double(matriz.count), height: screenWidth/Double(matriz[0].count))
+        VStack{
 
-                }
-                Text("").frame(width: screenWidth/Double(matriz.count), height: screenWidth/Double(matriz[0].count))
-            }
-            
             ForEach((0 ..<  matriz.count), id:\.self){ i in
                 
-                VStack(){
+                HStack(){
+                    Text(legendaColuna[i])
+                        .frame(width: screenWidth/Double(normalizador), height: screenWidth/Double(normalizador))
                     
                     ForEach((0 ..< matriz[i].count), id:\.self){ j in
                         
                             GeometryReader { geometry in
                           SquareView(
-                            quadrado: ModelSquare(valor: matriz[i][j], cor: Color.red, largura: screenWidth/Double(matriz.count), altura: screenWidth/Double(matriz.count)),showNumero: showNumero, escala: escala)
+                            quadrado: ModelSquare(valor: matriz[i][j], cor: Color.red, largura: screenWidth/Double(normalizador), altura: screenWidth/Double(normalizador)),showNumero: showNumero, escala: escala)
 
-                            }.frame(width: screenWidth/Double(matriz.count), height: screenWidth/Double(matriz[i].count))
-                    }
-                    
-                    Text(legendaColuna[i]).rotationEffect(Angle(degrees: -90))
-                        .frame(width: screenWidth/Double(matriz.count), height: screenWidth/Double(matriz[i].count))
+                            }.frame(width: screenWidth/Double(normalizador), height: screenWidth/Double(normalizador))
+                        }
                     }
 
                 }
+                
+            HStack{
+                
+                Text("").frame(width: screenWidth/Double(normalizador), height: screenWidth/Double(normalizador))
+                
+                ForEach(legendaLinha, id:\.self){ legenda in
+                    Text(legenda)
+                        .rotationEffect(Angle(degrees: -90))
+                        .frame(width: screenWidth/Double(normalizador), height: screenWidth/Double(normalizador))
+
+                }
+            }
             
             }
         
@@ -86,6 +90,6 @@ struct HeatmapGraph: View {
 
 struct HeatmapGraph_Previews: PreviewProvider {
     static var previews: some View {
-        HeatmapGraph(matriz: [[30,15,21], [14,8,29], [25,5,21]], showNumero: true, legendaColuna: ["roberto","Renatinho","Claudio"],legendaLinha: ["Cachorro","gato","peixe"])
+        HeatmapGraph(matriz: [[30,15,21,10], [28,2,14,15], [10,3,8,11]], showNumero: true, legendaColuna: ["roberto","Renatinho","Claudio","João"],legendaLinha: ["Cachorro","gato","peixe","Avestrus"])
     }
 }
